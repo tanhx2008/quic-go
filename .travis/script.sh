8a50f1ac2dce4b13b206edd0f2ab20b53b261f04
Lucas Clemente <lclemente@google.com>
2017-04-16 16:31:25 +0200

    Enable verbose mode for ginkgo on our CIs

--- .travis/script.sh
+++ .travis/script.sh
@@ -4,10 +4,10 @@ set -e
 
 go get -t ./...
 if [ ${TESTMODE} == "unit" ]; then
-  ginkgo -r --cover --randomizeAllSpecs --randomizeSuites --trace --progress --skipPackage integrationtests --skipMeasurements
+  ginkgo -v -r --cover --randomizeAllSpecs --randomizeSuites --trace --progress --skipPackage integrationtests --skipMeasurements
 fi
 
 if [ ${TESTMODE} == "integration" ]; then
-  ginkgo --randomizeAllSpecs --randomizeSuites --trace --progress -focus "Benchmark"
-  ginkgo -r --randomizeAllSpecs --randomizeSuites --trace --progress integrationtests
+  ginkgo -v --randomizeAllSpecs --randomizeSuites --trace --progress -focus "Benchmark"
+  ginkgo -v -r --randomizeAllSpecs --randomizeSuites --trace --progress integrationtests
 fi
